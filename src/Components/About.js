b2436229816f9508dd272d122fb092c9468b6fb2
GSCrawley <gscrawley@gmail.com>
2021-01-25 03:34:57 -0800

    .

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ class About extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var profilepic= './images/profilepic.jpg'.image;
+      var profilepic= './images/profilepic.jpg';
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
