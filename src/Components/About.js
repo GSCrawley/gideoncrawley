f9257062fda9281b72de18c1aea107cae33dbcb3
GSCrawley <gscrawley@gmail.com>
2021-01-25 04:15:16 -0800

    ..

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
             <img className="profilepic" src={profilepic} alt="Gideon Crawley Profile Pic" />
          </div>
          <div className="nine columns main-col">
-            <h2>About Me</h2>
+            <h2>About *  Me</h2>
 
             <p>{bio}</p>
             <div className="row">
