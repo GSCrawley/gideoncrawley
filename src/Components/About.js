99658b46acf5556eabc08c5d473eb263a25e4ac0
GSCrawley <gscrawley@gmail.com>
2021-01-25 04:14:20 -0800

    ..

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ class About extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      // var profilepic= './images/./profilepic.jpg';
+      var profilepic= './images/profilepic.jpg';
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
@@ -20,7 +20,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profilepic" src='./images/profilepic.jpg' alt="Gideon Crawley Profile Pic" />
+            <img className="profilepic" src={profilepic} alt="Gideon Crawley Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
