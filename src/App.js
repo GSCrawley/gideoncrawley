a0b76ce9a9a43453629290acc6553f00fa63ed17
Gideon Crawley <gscrawley@gmail.com>
2021-06-02 20:43:31 -0700

    rearranged in a way that makes a lot more sense and removed redundancies

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-import Contact from './Components/Contact';
+// import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
@@ -47,11 +47,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
         <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
+        {/* <Contact data={this.state.resumeData.main}/> */}
+        <About data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
