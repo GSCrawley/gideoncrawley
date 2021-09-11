94b599362a1fc0b6d19822d946693f1a37ac7f67
Gideon Crawley <gscrawley@gmail.com>
2021-09-11 02:40:33 -0700

    added updated resume and fixed positioning for stylin stylesheet

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -11,7 +11,7 @@ class Portfolio extends Component {
             <a href={projects.url} title={projects.title}>
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
-                  <div className="portfolio-item-meta">
+                  <div className="portfolio-item-meta" >
                  <h5>{projects.title}</h5>
                      <p>{projects.category}</p>
                   </div>
