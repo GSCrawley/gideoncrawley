90fe9076dc5fc363f421c8035e0f2a61a32484b6
Gideon Crawley <gscrawley@gmail.com>
2021-09-11 01:47:39 -0700

    Updates

--- precache-manifest.c91ead9940afd3795025b5547e970751.js
+++ precache-manifest.c91ead9940afd3795025b5547e970751.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "397c4a691d84e6fbb920",
+    "revision": "f6e33894cc31e26155b9",
     "url": "/gideoncrawley/static/css/main.07318036.chunk.css"
   },
   {
-    "revision": "397c4a691d84e6fbb920",
-    "url": "/gideoncrawley/static/js/main.86b48820.chunk.js"
+    "revision": "f6e33894cc31e26155b9",
+    "url": "/gideoncrawley/static/js/main.5251ed7b.chunk.js"
   },
   {
     "revision": "6c7363ed43ef73e54674",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/gideoncrawley/static/js/2.64c17f8e.chunk.js"
   },
   {
-    "revision": "b24909999c27a31be61bae0082d2ec7e",
+    "revision": "41c571cb9b19a9a51358dbbc5d67a789",
     "url": "/gideoncrawley/index.html"
   }
 ];
